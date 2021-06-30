93451e05d74e96fd5a3456096cc5bd68bb4c6ffc
Caitlin Bopp <boppcaitlin@gmail.com>
2021-06-29 22:42:02 -0400

    Populated api.js file in routes folder

--- routes/api.js
+++ routes/api.js
@@ -3,43 +3,50 @@ const Workout = require("../models/Workout.js");
 
 router.get("/api/workouts", (req, res) => {
     Workout.find()
+        .then(workouts => {
+            res.json(workouts);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        })
+});
+
+router.post("/api/workout", (req, res) => {
+    Workout.create({
+        day: Date.now()
+    })
+        .then(newWorkout => {
+            res.json(newWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.put("api/workouts/:id", (req, res) => {
+    Workout.findByIdAndUpdate(
+        req.params.id,
+        { $push: { exercises: req.body } },
+        { new: true }
+    )
+        .then(workout => {
+            res.json(workout)
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        })
+});
+
+router.get("api/workouts/range", (req, res) => {
+    Workout.find()
     .then(workouts => {
         res.json(workouts);
     })
     .catch(err => {
         res.status(400).json(err);
     })
-})
-
-router.post("/api/workout", ({ body }, res) => {
-  Workout.create(body)
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
 });
 
-// router.post("/api/transaction/bulk", ({ body }, res) => {
-//   Transaction.insertMany(body)
-//     .then(dbTransaction => {
-//       res.json(dbTransaction);
-//     })
-//     .catch(err => {
-//       res.status(400).json(err);
-//     });
-// });
 
-// router.get("/api/transaction", (req, res) => {
-//   Transaction.find({})
-//     .sort({ date: -1 })
-//     .then(dbTransaction => {
-//       res.json(dbTransaction);
-//     })
-//     .catch(err => {
-//       res.status(400).json(err);
-//     });
-// });
 
 module.exports = router;
--- routes/html.js
+++ routes/html.js
@@ -1,17 +1,20 @@
 const path = require('path');
-
-module.exports = (app) => {
-    app.get("/exercise", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/exercise.html"));
-    })
-
-    app.get("/stats", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/stats.html"));
-    })
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    })
+const router = require("express").Router();
 
 
-}
+router.get("/exercise", (req, res) => {
+    console.log("i've been clicked");
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+router.get("/stats", (req, res) => {
+    console.log("i've been clicked");
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+})
+
+
+module.exports = router;
